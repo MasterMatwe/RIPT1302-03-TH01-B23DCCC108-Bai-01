a42be3aef5c88015da21732b6ceb435008a4e15e
MasterMatwe <147083713+MasterMatwe@users.noreply.github.com>
2024-03-31 20:46:30 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const clockmain = document.getElementById("clock");
+function updateClock() {
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    const clocktext = `${hours}  :  ${minutes}  :  ${seconds}`;
+    clockmain.textContent = clocktext;
+}
+setInterval(updateClock, 1000);
+updateClock();
